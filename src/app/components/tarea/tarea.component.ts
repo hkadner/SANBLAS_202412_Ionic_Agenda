4cce31acf0bb9f8e7639d9e27ca553812b6df72d
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-27 11:31:27 +0100

    Estilos

--- src/app/components/tarea/tarea.component.ts
+++ src/app/components/tarea/tarea.component.ts
@@ -1,15 +1,51 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Tarea } from 'src/app/interfaces/tarea';
+import { Criticidad, Tarea } from 'src/app/interfaces/tarea';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from "@ionic/angular/standalone";
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-tarea',
   templateUrl: './tarea.component.html',
   styleUrls: ['./tarea.component.scss'],
+  standalone: true,
+  imports: [IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, NgStyle, NgClass]
 })
-export class TareaComponent  implements OnInit {
-  @Input() tarea : Tarea = {};
-  constructor() { }
+export class TareaComponent implements OnInit {
+  @Input() tarea: Tarea = {};
+  //Para ngStyle
+  estiloEstado = {}
+  estiloPendiente = { color: 'red' }
+  estiloEnProceso = { color: 'orange' }
+  estiloTerminada = { color: 'green' }
+  //Para ngClass
+  tipoCriticidad = '';
+  constructor() {
 
-  ngOnInit() {}
+  }
 
+  ngOnInit() {
+    switch (this.tarea.estado) {
+      case 'Pendiente':
+        this.estiloEstado = this.estiloPendiente;
+        break;
+      case 'En proceso':
+        this.estiloEstado = this.estiloEnProceso;
+        break;
+      case 'Finalizada':
+        this.estiloEstado = this.estiloTerminada;
+        break;
+    }
+
+    switch (this.tarea.criticidad) {
+      case Criticidad.BAJA:
+        this.tipoCriticidad = 'criticidad-baja';
+        break;
+      case Criticidad.MEDIA:
+        this.tipoCriticidad = 'criticidad-media';
+        break;
+      case Criticidad.ALTA:
+        this.tipoCriticidad = 'criticidad-alta';
+        break;
+    }
+  }
 }
--- src/app/services/tareas-manager.service.ts
+++ src/app/services/tareas-manager.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Tarea } from '../interfaces/tarea';
+import { Criticidad, Tarea } from '../interfaces/tarea';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +7,24 @@ import { Tarea } from '../interfaces/tarea';
 export class TareasManagerService {
 
   private tareas : Array<Tarea> = [];
-  constructor() { }
+  constructor() { 
+    let tareaFake : Tarea = {
+      nombre: 'Tarea fake',
+      descripcion: 'Esta es una tarea fake',
+      criticidad: Criticidad.BAJA,
+      estado: 'Finalizada',
+      fechaLimite : new Date().toISOString()
+    }
+    let tareaFake2 : Tarea = {
+      nombre: 'Tarea fake 2',
+      descripcion: 'Esta es una tarea fake 2',
+      criticidad: Criticidad.ALTA,
+      estado: 'En proceso',
+      fechaLimite : new Date().toISOString()
+    }
+    this.addTarea(tareaFake);
+    this.addTarea(tareaFake2);
+  }
   public addTarea(tarea: Tarea) {
     this.tareas.push({...tarea});//Para evitar duplicar la referencia
   }
